5672c3af6591b5ad875aaf04f0233783d6b61e38
Jacob Hampton <jhampton792@gmail.com>
2021-07-22 14:35:44 -0500

    Added endpoints for reading all users and creating a new user

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
+require('dotenv').config();
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
 const express = require('express');
 const app = express();
-require('dotenv').config();
+const jsonParser = express.json();
+
+const cors = require('cors');
+app.use(cors());
 
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Budgeters!");
+})
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany({})
+  res.json(users);
+})
+
+app.post('/users', jsonParser, async (req, res) => {
+
+  try {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email
+
+    const user = await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        email
+      }
+    });
+  
+    res.json(user);
+
+  } catch {
+    console.log(req.body);
+  }
+  
 })
 
 app.listen(port, () => {
